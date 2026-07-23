2a1854a88471581c7b2009ba1ed98e6a82d5ccd3
shambhavieee <shambhaviikumari11@gmail.com>
2026-07-23 12:29:39 +0530

    chore: change default region to ap-south-1 to match deployment bucket

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   type        = string
   description = "The AWS region to deploy resources in"
-  default     = "us-east-2"
+  default     = "ap-south-1"
 }
 
 variable "environment" {
@@ -26,21 +26,6 @@ variable "db_password" {
   type        = string
   description = "Database master password"
   sensitive   = true
-  default     = "VTLdataHash8899"
+  default     = "oms_postgres"
 }
 
-variable "vpc_subnet_ids" {
-  type        = list(string)
-  description = "Subnets to associate with RDS subnet group"
-  default     = [
-    "subnet-06f1debbf555ab7a4",
-    "subnet-0460a171c41f70be4",
-    "subnet-0278347682147b0f3"
-  ]
-}
-
-variable "vpc_id" {
-  type        = string
-  description = "The VPC ID to create the security group in"
-  default     = "vpc-0a95ee79c679996ce"
-}
